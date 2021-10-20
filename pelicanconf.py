58946f225bc939c509db283a4d85b6635acdb829
Romero <josea132.romero@gmail.com>
2021-10-20 18:25:14 -0400

    Added url of github pages for deployment

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,5 +1,5 @@
 AUTHOR = "Jose Ariel Romero"
-SITEURL = "/"
+SITEURL = "/coding-interview-questions"
 SITENAME = "Coding Interview Questions"
 SITETITLE = SITENAME
 SITESUBTITLE = "A portfolio with solution and explanation of all coding interview questions I have taken while applying for a job or practicing."
